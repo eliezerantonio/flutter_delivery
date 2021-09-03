7d38a0cc8f57eaebccee19eb1b5ac69513558aed
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-09-03 22:03:22 +0100

    usersProvider

--- flutter_delivery/lib/src/provider/users_provider.dart
+++ flutter_delivery/lib/src/provider/users_provider.dart
@@ -1,20 +1,37 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter_delivery/src/api/environment.dart';
 import 'package:flutter_delivery/src/models/response_api.dart';
 import 'package:flutter_delivery/src/models/user.dart';
 
 class UsersProvider {
-  
   String _url = Environment.API_DELIVERY;
   String _api = '/api/users';
 
-   BuildContext context;
+  BuildContext context;
 
   Future initState(BuildContext context) {
     this.context = context;
   }
 
-  Future<ResponseApi> create(User user){
-    
+  Future<ResponseApi> create(User user) async {
+    try {
+      Uri url = Uri.http(_url, "$_api/create");
+      String bodyParams = json.encode(user);
+
+      Map<String, dynamic> headers = {'Content-Type': 'application/json'};
+
+      final response = await http.post(url, headers: headers, body: bodyParams);
+
+      final data = json.decode(response.body);
+
+      ResponseApi responseApi = ResponseApi.fromJson(data);
+
+      return responseApi;
+    } catch (e) {
+      print("Error: $e");
+      return null;
+    }
   }
 }
