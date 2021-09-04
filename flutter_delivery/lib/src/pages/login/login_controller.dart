914d30edfb2a8b93e2d4a1b98f9501dc7bf5df78
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-09-04 23:22:14 +0100

    login

--- flutter_delivery/lib/src/pages/login/login_controller.dart
+++ flutter_delivery/lib/src/pages/login/login_controller.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_delivery/src/models/response_api.dart';
+import 'package:flutter_delivery/src/provider/users_provider.dart';
+import 'package:flutter_delivery/src/utils/my_snackbar.dart';
 
 class LoginController {
   BuildContext context;
@@ -6,8 +9,11 @@ class LoginController {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
-  Future init(BuildContext context) {
+  UsersProvider usersProvider = UsersProvider();
+
+  Future init(BuildContext context) async {
     this.context = context;
+    await usersProvider.initState(context);
   }
 
   void goToRegisterPage() {
@@ -16,8 +22,10 @@ class LoginController {
     }
   }
 
-  void login() {
+  void login() async {
     String email = emailController.text;
     String password = passwordController.text;
+    ResponseApi responseApi = await usersProvider.login(email, password);
+    MySnackbar.show(context, responseApi.message ?? "");
   }
 }
