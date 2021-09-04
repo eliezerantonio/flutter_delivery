ceeed17501a1b25bcdc473bc780c650a93d259ba
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-09-04 22:48:44 +0100

    metodo login

--- flutter_delivery/lib/src/provider/users_provider.dart
+++ flutter_delivery/lib/src/provider/users_provider.dart
@@ -20,16 +20,31 @@ class UsersProvider {
       Uri url = Uri.http(_url, "$_api/create");
       String bodyParams = json.encode(user);
 
-     
-
       Map<String, String> headers = {'Content-Type': 'application/json'};
 
       final response = await http.post(url, headers: headers, body: bodyParams);
- print(response);
+
       final data = json.decode(response.body);
 
       ResponseApi responseApi = ResponseApi.fromJson(data);
 
+      return responseApi;
+    } catch (e) {
+      print("Error: $e");
+      return null;
+    }
+  }
+
+  Future<ResponseApi> login(String email, String password) async {
+    try {
+      Uri url = Uri.http(_url, '$_api/login');
+      String bodyParams = json.encode({'email': email, 'password': password});
+
+      Map<String, String> headers = {'Content-Type': 'application/json'};
+
+      final res = await http.post(url, headers: headers, body: bodyParams);
+      final data = json.decode(res.body);
+      ResponseApi responseApi = new ResponseApi();
       return responseApi;
     } catch (e) {
       print("Error: $e");
