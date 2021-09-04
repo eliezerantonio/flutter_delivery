3b1cadbfcf99945db38d2798cd4f1fe85f3422b6
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-09-05 00:18:21 +0100

    SharedPrefs metodos

---
+++ flutter_delivery/lib/src/utils/shared_prefs.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPref {
+  Future<void> save(String key, dynamic value) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString(key, json.encode(value));
+  }
+
+  Future<void> read(String key, dynamic value) async {
+    final prefs = await SharedPreferences.getInstance();
+    if (prefs.getString(key) == null) return null;
+
+    return json.decode(prefs.getString(key));
+  }
+
+  Future<bool> contains(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.containsKey(key);
+  }
+
+  Future<bool> remove(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.remove(key);
+  }
+}
