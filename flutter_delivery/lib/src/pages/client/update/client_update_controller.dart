685c38df1bab0bb7a7a62893e82ddb7bcbe26c9a
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-09-07 06:32:20 +0100

    cliente update

---
+++ flutter_delivery/lib/src/pages/client/update/client_update_controller.dart
@@ -0,0 +1,133 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_delivery/src/models/response_api.dart';
+import 'package:flutter_delivery/src/models/user.dart';
+import 'package:flutter_delivery/src/provider/users_provider.dart';
+import 'package:flutter_delivery/src/utils/my_snackbar.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:sn_progress_dialog/progress_dialog.dart';
+
+class ClientUpdateController {
+  BuildContext context;
+  //controladores
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController lastnameController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+
+  UsersProvider usersProvider = UsersProvider();
+  XFile pickedFile;
+  File imageFile;
+  Function refresh;
+  ProgressDialog _progressDialog;
+  bool isLoading;
+  Future init(BuildContext context, Function refresh) {
+    this.context = context;
+    usersProvider.initState(context);
+    this.refresh = refresh;
+    _progressDialog = ProgressDialog(context: context);
+  }
+
+  void backToLoginPage() {
+    Navigator.pop(context);
+  }
+
+  void register() async {
+    String email = emailController.text.trim();
+    String password = passwordController.text.trim();
+    String name = nameController.text.trim();
+    String lastname = lastnameController.text.trim();
+    String confirmPassword = confirmPasswordController.text.trim();
+    String phone = phoneController.text.trim();
+
+    if (email.isEmpty ||
+        password.isEmpty ||
+        confirmPassword.isEmpty ||
+        phone.isEmpty ||
+        name.isEmpty ||
+        lastname.isEmpty) {
+      MySnackbar.show(context, "Preencha todos campos");
+      return;
+    }
+    if (password.isEmpty != confirmPassword.isEmpty) {
+      MySnackbar.show(context, "Senhas Diferentes");
+      return;
+    }
+    if (password.length < 6) {
+      MySnackbar.show(context, "Senha muito curta");
+      return;
+    }
+
+    if (imageFile == null) {
+      MySnackbar.show(context, "Selecione uma imagem");
+    }
+    _progressDialog.show(max: 100, msg: "Realizando o cadastro");
+    isLoading = true;
+    User user = User(
+        email: email,
+        password: password,
+        name: name,
+        lastname: lastname,
+        phone: phone);
+
+    Stream stream = await usersProvider.createWithImage(user, imageFile);
+    stream.listen((res) {
+      // ResponseApi responseApi=await usersProvider.create(user);
+      _progressDialog.close();
+      isLoading = false;
+      ResponseApi responseApi = ResponseApi.fromJson(json.decode(res));
+
+      MySnackbar.show(context, responseApi.message);
+
+      if (responseApi.success) {
+        Future.delayed(Duration(seconds: 3), () {
+          Navigator.pushNamed(context, 'login');
+        });
+      } else {
+        isLoading = false;
+      }
+    });
+  }
+
+  void showAlertDialog() {
+    Widget galleryButton = ElevatedButton(
+      onPressed: () {
+        selectImage(ImageSource.gallery);
+      },
+      child: Text("Galeria"),
+    );
+
+    Widget cameraButton = ElevatedButton(
+      onPressed: () {
+        selectImage(ImageSource.camera);
+      },
+      child: Text("Camera"),
+    );
+
+    AlertDialog alertDialog = AlertDialog(
+      title: Text("Selecionar imagem"),
+      actions: [
+        galleryButton,
+        cameraButton,
+      ],
+    );
+    showDialog(
+        context: context,
+        builder: (context) {
+          return alertDialog;
+        });
+  }
+
+  Future selectImage(ImageSource imageSource) async {
+    pickedFile = await ImagePicker().pickImage(source: imageSource);
+    if (pickedFile != null) {
+      imageFile = File(pickedFile.path);
+    }
+    Navigator.of(context).pop();
+    refresh();
+  }
+}
---
+++ flutter_delivery/lib/src/pages/client/update/client_update_page.dart
@@ -0,0 +1,267 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_delivery/src/pages/client/update/client_update_controller.dart';
+import 'package:flutter_delivery/src/utils/my_colors.dart';
+
+class ClientUpdatePage extends StatefulWidget {
+  ClientUpdatePage({Key key}) : super(key: key);
+
+  @override
+  _ClientUpdatePageState createState() => _ClientUpdatePageState();
+}
+
+class _ClientUpdatePageState extends State<ClientUpdatePage> {
+  ClientUpdateController _controller = ClientUpdateController();
+  @override
+  void initState() {
+    super.initState();
+    SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+      _controller.init(context, refresh);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        child: Stack(
+          children: [
+            SingleChildScrollView(
+              physics: BouncingScrollPhysics(),
+              child: Column(
+                children: [
+                  _imageUser(),
+                  _texFieldEmail(),
+                  _texFieldName(),
+                  _texFieldLastName(),
+                  _texFieldLastPhone(),
+                  _texFieldPassword(),
+                  _texFieldConfirmPassword(),
+                  _buttonLogin(),
+                ],
+              ),
+            ),
+            Positioned(
+              left: -100,
+              top: -80,
+              child: _circleLogin(),
+            ),
+            Positioned(
+              top: 60,
+              left: 10,
+              child: _textRegister(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _circleLogin() {
+    return Container(
+      width: 240,
+      height: 230,
+      decoration: BoxDecoration(
+        color: Colors.red,
+        borderRadius: BorderRadius.circular(100),
+      ),
+    );
+  }
+
+  Widget _imageUser() {
+    return GestureDetector(
+      onTap: _controller.showAlertDialog,
+      child: Container(
+        margin: EdgeInsets.only(
+          top: 130,
+          bottom: MediaQuery.of(context).size.height * 0.07,
+        ),
+        child: CircleAvatar(
+          backgroundImage: _controller.imageFile != null
+              ? FileImage(_controller.imageFile)
+              : AssetImage("assets/img/avatar.png"),
+          radius: 55,
+        ),
+      ),
+    );
+  }
+
+  Widget _textRegister() {
+    return GestureDetector(
+      onTap: () {
+        _controller.backToLoginPage();
+      },
+      child: Row(
+        children: [
+          Icon(Icons.arrow_back_ios, color: Colors.white),
+          Text("REGISTRO",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+                fontSize: 18,
+                fontFamily: 'NimbusSans',
+              )),
+        ],
+      ),
+    );
+  }
+
+  Widget _buttonLogin() {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.symmetric(horizontal: 55, vertical: 20),
+      child: ElevatedButton(
+        onPressed: _controller.isLoading ? null : _controller.register,
+        child: Text("Cadastrar"),
+        style: ElevatedButton.styleFrom(
+          primary: MyColors.primaryColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          padding: EdgeInsets.symmetric(vertical: 15),
+        ),
+      ),
+    );
+  }
+
+  Widget _texFieldEmail() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 7),
+      decoration: BoxDecoration(
+          color: MyColors.primaryOpacityColor,
+          borderRadius: BorderRadius.circular(30)),
+      child: TextField(
+        controller: _controller.emailController,
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          hintText: 'E-mail',
+          border: InputBorder.none,
+          hintStyle: TextStyle(color: MyColors.primaryColorDark),
+          prefixIcon: Icon(
+            Icons.email,
+            color: MyColors.primaryColor,
+          ),
+          contentPadding: EdgeInsets.all(15),
+        ),
+      ),
+    );
+  }
+
+  Widget _texFieldName() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 7),
+      decoration: BoxDecoration(
+          color: MyColors.primaryOpacityColor,
+          borderRadius: BorderRadius.circular(30)),
+      child: TextField(
+        controller: _controller.nameController,
+        decoration: InputDecoration(
+          hintText: 'Nome',
+          border: InputBorder.none,
+          hintStyle: TextStyle(color: MyColors.primaryColorDark),
+          prefixIcon: Icon(
+            Icons.person,
+            color: MyColors.primaryColor,
+          ),
+          contentPadding: EdgeInsets.all(15),
+        ),
+      ),
+    );
+  }
+
+  Widget _texFieldLastName() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 7),
+      decoration: BoxDecoration(
+          color: MyColors.primaryOpacityColor,
+          borderRadius: BorderRadius.circular(30)),
+      child: TextField(
+        controller: _controller.lastnameController,
+        decoration: InputDecoration(
+          hintText: 'Apelido',
+          border: InputBorder.none,
+          hintStyle: TextStyle(color: MyColors.primaryColorDark),
+          prefixIcon: Icon(
+            Icons.person_rounded,
+            color: MyColors.primaryColor,
+          ),
+          contentPadding: EdgeInsets.all(15),
+        ),
+      ),
+    );
+  }
+
+  Widget _texFieldLastPhone() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 7),
+      decoration: BoxDecoration(
+          color: MyColors.primaryOpacityColor,
+          borderRadius: BorderRadius.circular(30)),
+      child: TextField(
+        controller: _controller.phoneController,
+        keyboardType: TextInputType.phone,
+        decoration: InputDecoration(
+          hintText: 'Telefone',
+          border: InputBorder.none,
+          hintStyle: TextStyle(color: MyColors.primaryColorDark),
+          prefixIcon: Icon(
+            Icons.phone,
+            color: MyColors.primaryColor,
+          ),
+          contentPadding: EdgeInsets.all(15),
+        ),
+      ),
+    );
+  }
+
+  Widget _texFieldPassword() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 7),
+      decoration: BoxDecoration(
+          color: MyColors.primaryOpacityColor,
+          borderRadius: BorderRadius.circular(30)),
+      child: TextField(
+        controller: _controller.passwordController,
+        obscureText: true,
+        decoration: InputDecoration(
+          hintText: 'Senha',
+          border: InputBorder.none,
+          hintStyle: TextStyle(color: MyColors.primaryColorDark),
+          prefixIcon: Icon(
+            Icons.lock,
+            color: MyColors.primaryColor,
+          ),
+          contentPadding: EdgeInsets.all(15),
+        ),
+      ),
+    );
+  }
+
+  Widget _texFieldConfirmPassword() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 7),
+      decoration: BoxDecoration(
+          color: MyColors.primaryOpacityColor,
+          borderRadius: BorderRadius.circular(30)),
+      child: TextField(
+        controller: _controller.confirmPasswordController,
+        obscureText: true,
+        decoration: InputDecoration(
+          hintText: 'Confrima senha',
+          border: InputBorder.none,
+          hintStyle: TextStyle(color: MyColors.primaryColorDark),
+          prefixIcon: Icon(
+            Icons.lock_open,
+            color: MyColors.primaryColor,
+          ),
+          contentPadding: EdgeInsets.all(15),
+        ),
+      ),
+    );
+  }
+
+  void refresh() {
+    setState(() {});
+  }
+}
