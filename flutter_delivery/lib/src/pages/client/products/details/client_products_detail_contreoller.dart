5ff561f78cb38dc2207f37b32ccee2e9f5bfb8f3
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-09-16 16:02:57 +0100

    metodos add e remove

--- flutter_delivery/lib/src/pages/client/products/details/client_products_detail_contreoller.dart
+++ flutter_delivery/lib/src/pages/client/products/details/client_products_detail_contreoller.dart
@@ -6,11 +6,31 @@ class ClientProductsDetailController {
 
   Function refresh;
   Product product;
+
+  int counter = 1;
+  double productPrice;
+
   Future init(BuildContext context, Function refresh, Product product) {
     this.context = context;
     this.refresh = refresh;
     this.product = product;
 
     refresh();
+  }
+
+  void addItem() {
+    counter = counter + 1;
+    productPrice = product.price * counter;
+    product.quantity = counter;
+    refresh();
+  }
+
+  void removeItem() {
+    if (counter > 1) {
+      counter = counter - 1;
+      productPrice = product.price * counter;
+      product.quantity = counter;
+      refresh();
+    }
   }
 }
